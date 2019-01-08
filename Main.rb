ebffad0966ab54c7c1073767220930dbcf9a53af
MIngjianZhang <mzhang41@u.rochester.edu>
2019-01-08 04:37:40 -0500

    update

--- Main.rb
+++ Main.rb
@@ -25,7 +25,7 @@ Doc
     puts INFO
 	end
  
-	def start(x_player, o_player, board)
+	def start
     puts "\n >> PLEASE SEE THE POSITIONS OF THE BOARD << \n\n"
 
     print "Type what the board will look like start with the width: "
@@ -36,7 +36,11 @@ Doc
     boardheight = gets.chomp
     puts boardheight
 
-    print "Finally, do you want your AI to be a moron or a guru? (type as it is please)"
+    print"alright, how many in a line (any direction) should one be considered winning? (do not be bigger than height or width) "
+    boardWinCount = gets.chomp
+    puts boardWinCount
+    
+    print "Finally, do you want your AI to be a moron or a guru? (1 for moron 2 for guru)"
     aitype = gets.chomp
     puts aitype
 	end
@@ -46,10 +50,12 @@ main.display_info
 =begin
 x_player = Player.new("X")
 o_player = Player.new("O") # This is a robot :)
-=end
-boardmanage    = BoardManage.new
+boardmanage= BoardManage.new
 boardmanage.display_positions
+=end
 
-engine.start(x_player, o_player, board) # this will start game and
+
+engine.start
                                         # automatically terminate the game if player wins the game
 engine.stop # if match is draw
+end
